053529b4b841da06e052f09485e1d5d0b78d31e1
ph3ntom <lsjkeh9683@naver.com>
2025-09-20 21:15:16 +0900

    db 에러, sql 인젝션 추가 0920

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RegisterController } from './register/register.controller';
 import { LoginController } from './login/login.controller';
+import { UsersController } from './users/users.controller';
 import { LoginService } from './login/login.service';
+import { UsersService } from './users/users.service';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RegisterService } from './register/register.service';
@@ -9,7 +11,7 @@ import { UserService } from '../sql/auth/auth';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [RegisterController, LoginController],
-  providers: [LoginService, RegisterService, UserService],
+  controllers: [RegisterController, LoginController, UsersController],
+  providers: [LoginService, RegisterService, UserService, UsersService],
 })
 export class AuthModule {}
